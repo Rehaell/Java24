e52fe0528ca33e575b152b4bf092d0a002d0264b
Rui Romão <rehaellspam@gmail.com>
2013-09-20 11:20:01 +0100

    final

--- Java24/src/RandomizeNum.java
+++ Java24/src/RandomizeNum.java
@@ -5,6 +5,7 @@ public class RandomizeNum {
 	
 	public int[] random(int arrayLength, int maximo) {
 		int[] arrayNum = new int[arrayLength];
+		boolean numRepetido = false;
 		
 		
 		for(int i = 0; i < arrayNum.length; i++){
@@ -14,18 +15,24 @@ public class RandomizeNum {
 			while(true){
 				if (randNum < 0)
 					randNum = randNum * -1;
-				if(Arrays.asList(arrayNum).contains(randNum)){
-					randNum = 1 + (int)(Math.random() * maximo );
+				for(int j = 0; j < arrayNum.length ; j++){
+					if(arrayNum[j] == randNum){
+						numRepetido = true;
+						break;
+					} else 
+						numRepetido = false;
+				}
+				if(numRepetido == true){
+					randNum = 1 + 	(int)(Math.random() * maximo );
 					System.out.print("O numero gerado foi repetido, gerado um novo  : " + randNum + "\n");
 				} else{
-
 					arrayNum[i] = randNum;
 					System.out.print("O numero foi escrito no Array : " + arrayNum[i] + "\n");
 					break;
 				}
 			}					
 		}
-		
+		Arrays.sort(arrayNum);
 		return arrayNum;
 		
 		
